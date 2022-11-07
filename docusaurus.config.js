2a42dd7cf5cccdaeff925a3cde881566b11173ba
cristianopolicarpo <cristiano.policarpo@baloom.co>
2022-11-07 20:19:08 -0300

    added doc

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/wireshape/wireshape-org/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/wireshape/wireshape-org/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -68,12 +68,12 @@ const config = {
           src: 'img/ws-logo.png',
         },
         items: [
-          // {
-          //   type: 'doc',
-          //   docId: 'introduction/introduction',
-          //   position: 'right',
-          //   label: 'Tutorial',
-          // },
+          {
+            type: 'doc',
+            docId: 'quick-start/introduction',
+            position: 'right',
+            label: 'Documentation',
+          },
           // {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/wireshape',
--- src/components/ProductHuntCard.jsx
+++ src/components/ProductHuntCard.jsx
@@ -7,14 +7,14 @@ export default function ProductHuntCard({ className, style }) {
     const src = `https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=356907&theme=${theme}`;
     return (
         <a
-            href="https://www.producthunt.com/posts/crawlee"
+            href="https://www.producthunt.com/posts/wireshape"
             target="_blank"
             rel="noreferrer"
             className={className}
             style={{ display: 'block', width: 250, height: 54, ...style }}>
             <img
                 src={src}
-                alt="Crawlee - Crawlee helps you build reliable crawlers&#0046; Fast&#0046; | Product Hunt"
+                alt="Wireshape - Open Product Data Blockchain&#0046; Fast&#0046; | Product Hunt"
                 style={{ width: 250, height: 54, maxWidth: 'initial' }}
                 width={250}
                 height={54}
